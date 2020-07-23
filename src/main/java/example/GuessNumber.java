3ef44d33bab2e1f631bf579ea7c99d6c7f0429d3
Jenkin Tang <jenkincs@163.com>
2020-07-23 23:19:49 +0800

    [guess number][Jenkin] feature:pass method should_return_0A0B_when_guess_given_answer_1234_and_input_7856 in class GuessNumber

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
  * @date 2020/7/23 - 22:12
  */
 public class GuessNumber {
+
     public String guess(LinkedList<Integer> answer,LinkedList<Integer> inputNumber){
         if(answer.equals(inputNumber)){
             return "4A0B";
@@ -16,7 +17,22 @@ public class GuessNumber {
                 isHaveZeroCorrectNumberWithUnCorrectPosition(answer,inputNumber)){
             return "2A0B";
         }
+        if(isHaveNoCorrectNumber(answer,inputNumber)){
+            return "0A0B";
+        }
         return null;
+    }
+
+    public boolean isHaveNoCorrectNumber(LinkedList<Integer> answer,
+                                         LinkedList<Integer> inputNumber){
+        HashSet<Integer> unionOfNumber = new HashSet<>();
+        unionOfNumber.addAll(answer);
+        unionOfNumber.addAll(inputNumber);
+        if(unionOfNumber.size() == 8){
+            return true;
+        }
+        return false;
+
     }
 
     public boolean isHaveTwoCorrectNumberWithCorrectPosition(LinkedList<Integer> answer,
