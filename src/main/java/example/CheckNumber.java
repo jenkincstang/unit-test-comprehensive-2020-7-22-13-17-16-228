d9d02e45d94d00633129826224f951db4fb2bf45
jenkin <jenkincs@163.com>
2020-07-27 22:43:14 +0800

    [guess number][jenkin] feature: pass

--- src/main/java/example/CheckNumber.java
+++ src/main/java/example/CheckNumber.java
@@ -3,6 +3,7 @@ package example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class CheckNumber {
@@ -10,10 +11,22 @@ public class CheckNumber {
     public boolean isLegalNumber(LinkedList<Integer> inputNumbers){
         if(inputNumbers.size() == 4)return true;
         if(isNumberInRange(inputNumbers))return true;
+        if(isNotContainsRepeatElement(inputNumbers))return true;
+
         return false;
     }
+
     public boolean isNumberInRange(LinkedList<Integer> list){
         return list.stream().filter(i -> i>9 || i<0).count() == 0;
+    }
+
+    public boolean isNotContainsRepeatElement(LinkedList<Integer> list){
+        HashSet<Integer> set = new HashSet();
+        for (Integer item:list){
+            set.add(item);
+        }
+        if(set.size() == list.size()) return true;
+        return false;
     }
 
 }
