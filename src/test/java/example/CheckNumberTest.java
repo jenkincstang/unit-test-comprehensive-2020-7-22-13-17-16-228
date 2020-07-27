51d2c656bd27baeef1f04f1ba8d93b42a5a5c47f
jenkin <jenkincs@163.com>
2020-07-27 22:39:33 +0800

    [guess number][jenkin] test: test should_return_true_when_judge_number_not_repeat_given_5_number_without_repeat

--- src/test/java/example/CheckNumberTest.java
+++ src/test/java/example/CheckNumberTest.java
@@ -26,7 +26,7 @@ public class CheckNumberTest {
     }
 
     @Test
-    public void should_return_true_when_judge_number_range_given_4_number_between_0_and_9() {
+    public void should_return_true_when_judge_number_range_given_number_between_0_and_9() {
         //given
         CheckNumber checkNumber = new CheckNumber();
         //when
@@ -42,4 +42,20 @@ public class CheckNumberTest {
         Assertions.assertEquals(true,result);
     }
 
+    @Test
+    public void should_return_true_when_judge_number_not_repeat_given_5_number_without_repeat() {
+        //given
+        CheckNumber checkNumber = new CheckNumber();
+        //when
+        LinkedList<Integer> inputNumbers = new LinkedList<>();
+        inputNumbers.add(1);
+        inputNumbers.add(2);
+        inputNumbers.add(8);
+        inputNumbers.add(9);
+        inputNumbers.add(10);
+
+        boolean result = checkNumber.isLegalNumber(inputNumbers);
+        //then
+        Assertions.assertEquals(true,result);
+    }
 }
