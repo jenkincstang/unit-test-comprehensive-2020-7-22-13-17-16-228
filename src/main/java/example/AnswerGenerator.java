b479394cfd71b076b6fe4d17d331f2623a21128b
jenkin <jenkincs@163.com>
2020-07-28 01:37:54 +0800

    [guess number][jenkin] refactor: refactor getRandomNumbers method in class AnswerGenerator

--- src/main/java/example/AnswerGenerator.java
+++ src/main/java/example/AnswerGenerator.java
@@ -9,19 +9,17 @@ public class AnswerGenerator {
 
     private LinkedList<Integer> getRandomNumbers() {
         LinkedList<Integer> answer = new LinkedList<>();
-        Random randomNumberGenerator = new Random();
-        Integer[] optionalNumbers = {0,1,2,4,5,6,7,8,9};
-        List<Integer> optionalNumberList = new ArrayList<>(Arrays.asList(optionalNumbers));
-        int randomBound = 9;
+        List<Integer> optionalNumbers = new ArrayList<Integer>(Arrays.asList(new Integer[]{0, 1, 2, 4, 5, 6, 7, 8, 9}));
 
+        Random randomIndexGenerator = new Random();
+        int randomBound = 9;
         for (int quantityOfRandomNumber = 0; quantityOfRandomNumber< 4; quantityOfRandomNumber++){
-            int randomIndex = randomNumberGenerator.nextInt(randomBound);
-            Integer randomNumber = optionalNumberList.get(randomIndex);
+            int randomIndex = randomIndexGenerator.nextInt(randomBound);
+            Integer randomNumber = optionalNumbers.get(randomIndex);
             answer.add(randomNumber);
-            optionalNumberList.remove(randomIndex);
-            randomBound--;
+            optionalNumbers.remove(randomIndex);
+            randomBound --;
         }
-        System.out.println(answer);
         return answer;
     }
 }
