c7f4d3ef2c9bca1dba5c87124600a0d4592b02e6
jenkin <jenkincs@163.com>
2020-07-27 20:11:34 +0800

    [guess number][jenkin] test: test should_return_true_when_genarate_answer

--- src/test/java/example/AnswerGeneratorTest.java
+++ src/test/java/example/AnswerGeneratorTest.java
@@ -19,6 +19,7 @@ public class AnswerGeneratorTest {
         Assertions.assertEquals(4,resultList.size());
     }
 
+
     @Test
     public void should_return_true_when_genarate_answer(){
         //given
@@ -29,6 +30,7 @@ public class AnswerGeneratorTest {
         //then
         Assertions.assertEquals(true,isNotContainsRepeatElement(resultList));
     }
+
 
     public boolean isNotContainsRepeatElement(LinkedList<Integer> list){
         HashSet<Integer> set = new HashSet();
