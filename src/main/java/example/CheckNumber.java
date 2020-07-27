b00adcee59c3f0a652010d4acac6b7d77f375d3f
jenkin <jenkincs@163.com>
2020-07-27 22:35:13 +0800

    [guess number][jenkin] feature: pass should_return_true_when_judge_number_range_given_4_number_between_0_and_9

--- src/main/java/example/CheckNumber.java
+++ src/main/java/example/CheckNumber.java
@@ -9,6 +9,11 @@ public class CheckNumber {
 
     public boolean isLegalNumber(LinkedList<Integer> inputNumbers){
         if(inputNumbers.size() == 4)return true;
+        if(isNumberInRange(inputNumbers))return true;
         return false;
     }
+    public boolean isNumberInRange(LinkedList<Integer> list){
+        return list.stream().filter(i -> i>9 || i<0).count() == 0;
+    }
+
 }
