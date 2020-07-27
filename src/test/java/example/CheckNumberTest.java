243a30a16ab980b143127adb72f9f5e90e626793
jenkin <jenkincs@163.com>
2020-07-27 22:32:52 +0800

    [guess number][jenkin] test: test should_return_true_when_judge_number_range_given_4_number_between_0_and_9

--- src/test/java/example/CheckNumberTest.java
+++ src/test/java/example/CheckNumberTest.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 
 public class CheckNumberTest {
     @Test
-    public void should_return_true_when_count_quantity_of_number_given_4_number() throws IOException {
+    public void should_return_true_when_count_quantity_of_number_given_4_number() {
         //given
         CheckNumber checkNumber = new CheckNumber();
         //when
@@ -25,5 +25,21 @@ public class CheckNumberTest {
         Assertions.assertEquals(true,result);
     }
 
+    @Test
+    public void should_return_true_when_judge_number_range_given_4_number_between_0_and_9() {
+        //given
+        CheckNumber checkNumber = new CheckNumber();
+        //when
+        LinkedList<Integer> inputNumbers = new LinkedList<>();
+        inputNumbers.add(1);
+        inputNumbers.add(2);
+        inputNumbers.add(2);
+        inputNumbers.add(8);
+        inputNumbers.add(9);
+
+        boolean result = checkNumber.isLegalNumber(inputNumbers);
+        //then
+        Assertions.assertEquals(true,result);
+    }
 
 }
