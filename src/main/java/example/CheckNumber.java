e936298ee243f6f4e31340ffbe0f073f6f5fa785
jenkincstang <jenkincs@163.com>
2020-08-01 14:50:49 +0800

    refactor: refactor CheckNumber class

--- src/main/java/example/CheckNumber.java
+++ src/main/java/example/CheckNumber.java
@@ -2,10 +2,11 @@ package example;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 
 public class CheckNumber {
 
-    public boolean isLegalNumber(LinkedList<Integer> inputNumbers){
+    public boolean isLegalNumber(List<Integer> inputNumbers){
         int legalQuantityOfInputNumber = 4;
         if(inputNumbers.size() != legalQuantityOfInputNumber)return false;
         if(!isNumberInRange(inputNumbers))return false;
@@ -13,11 +14,11 @@ public class CheckNumber {
         return true;
     }
 
-    public boolean isNumberInRange(LinkedList<Integer> list){
+    public boolean isNumberInRange(List<Integer> list){
         return list.stream().filter(i -> i>9 || i<0).count() == 0;
     }
 //TODO
-    public boolean isNotContainsRepeatElement(LinkedList<Integer> elements){
+    public boolean isNotContainsRepeatElement(List<Integer> elements){
         return elements.stream().distinct().count() == elements.size();
     }
 
--- src/main/java/example/GameProcess.java
+++ src/main/java/example/GameProcess.java
@@ -3,7 +3,12 @@ package example;
 import java.util.List;
 
 public class GameProcess {
+    private CheckNumber checkNumber;
+    public GameProcess(){
+        checkNumber = new CheckNumber();
+    }
     public String start(List<Integer> inputs) {
+        checkNumber.isLegalNumber(inputs);
         return null;
     }
 }
