00fd0996222686e911dbf017d7093c6c96feea2a
jenkincstang <jenkincs@163.com>
2020-08-01 19:00:43 +0800

    [Guess Number][jenkin] feature: pass should_return_1234_when_receive_number_from_console_given_1234

--- src/main/java/example/InputReceivement.java
+++ src/main/java/example/InputReceivement.java
@@ -1,9 +1,14 @@
 package example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputReceivement {
-    public List<Integer> receive(String input) {
-        return null;
+    public List<Integer> receive(String consoleInput) {
+        List<Integer> inputs = new ArrayList<>();
+        for(String number:consoleInput.split(" ")){
+            inputs.add(Integer.valueOf(number));
+        }
+        return inputs;
     }
 }
